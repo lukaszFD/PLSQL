be15c16810113b8e73d3e083fd232ae8e39c5ee2
lukaszFD <lukasz.f.d@gmail.com>
2019-01-03 15:51:02 +0100

    new

--- PL_SQL_Examples/CA_GetDataFromOracleServer/Program.cs
+++ PL_SQL_Examples/CA_GetDataFromOracleServer/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Data;
 
 namespace CA_GetDataFromOracleServer
 {
@@ -10,6 +8,41 @@ namespace CA_GetDataFromOracleServer
     {
         static void Main(string[] args)
         {
+            string oradb = "Data Source=(DESCRIPTION="
+                         + "(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521))"
+                         + "(CONNECT_DATA=(SERVICE_NAME=xe)));"
+                         + "User Id=test;Password=password;";
+            OracleConnection conn = new OracleConnection(oradb);
+            try
+            {
+                conn.Open();
+                
+                OracleCommand cmd = new OracleCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "select  * from employees_test";
+                cmd.CommandType = CommandType.Text;
+                OracleDataReader dr = cmd.ExecuteReader();
+                var dataTable = new DataTable();
+                dataTable.Load(dr);
+
+                for (int i = 0; i < dataTable.Rows.Count; i++)
+                {
+                    Console.WriteLine(dataTable.Rows[i]["first_name"].ToString() + " " + dataTable.Rows[i]["last_name"].ToString());
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
--- PL_SQL_Examples/CA_InsertDataIntoOracleServer/Program.cs
+++ PL_SQL_Examples/CA_InsertDataIntoOracleServer/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace CA_InsertDataIntoOracleServer
 {
@@ -10,6 +12,19 @@ namespace CA_InsertDataIntoOracleServer
     {
         static void Main(string[] args)
         {
+
+            var doc = XDocument.Load(@"D:\XML_Test\1.xml");
+
+            string sourceFileName = "";
+            foreach (var item in Directory.GetFiles(@"D:\XML_Test\", "*.xml"))
+            {
+                sourceFileName = item.ToString();
+                Console.WriteLine(sourceFileName);
+            }
+
+
+
+            Console.ReadKey();
         }
     }
 }
