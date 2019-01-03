bac7073dc29069dd1b73920ceaac17d5e2fcc15f
lukasz.f.d@gmail.con <>
2019-01-03 15:19:17 +0100

    New design

--- Table/tbl_Customers.sql
+++ Table/tbl_Customers.sql
@@ -1,7 +1,7 @@
 create table tbl_Customers
 (
-    CustomerID number(38) CONSTRAINT cus_id primary key not null,
-    ExternalID varchar2(32) default SYS_GUID() not null,
-    DateFrom date default sysdate not null,
-    DateTo date 
+    Customer_ID number(38) CONSTRAINT cus_id primary key not null,
+    External_ID varchar2(32) default SYS_GUID() not null,
+    Date_From date default sysdate not null,
+    Date_To date default TO_DATE('31.12.9999 23:59:59', 'dd/mm/yyyy hh24:mi:ss')
 );
