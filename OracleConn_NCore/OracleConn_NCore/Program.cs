d9b82147e7616ff07e1eabfefae5fe6de3234fe0
lukaszFD <lukasz.f.d@gmail.com>
2019-04-09 22:51:30 +0200

    Oracle Data Provider for .NET Core

---
+++ OracleConn_NCore/OracleConn_NCore/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Oracle.ManagedDataAccess.Client;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {	
+            string conString = "User Id=hr;Password=1234;Data Source=localhost:1521/XE;";
+
+            using (OracleConnection con = new OracleConnection(conString))
+            {
+                using (OracleCommand cmd = con.CreateCommand())
+                {
+                    try
+                    {
+                        con.Open();
+                        cmd.BindByName = true;
+                        cmd.CommandText = "select first_name from employees where department_id = :id";
+                        OracleParameter id = new OracleParameter("id", 50);
+                        cmd.Parameters.Add(id);
+                        OracleDataReader reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            Console.WriteLine("Employee First Name: " + reader.GetString(0));
+                        }
+
+                        Console.WriteLine();
+                        Console.WriteLine("Press 'Enter' to continue");
+
+                        reader.Dispose();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
