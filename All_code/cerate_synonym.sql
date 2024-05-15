29cb0bce750a8c71cf2207fd6a63e9336cbbffdf
Łukasz <lukasz.f.d@gmail.com>
2024-05-15 10:00:29 +0200

    Update cerate_synonym.sql

--- All_code/cerate_synonym.sql
+++ All_code/cerate_synonym.sql
@@ -1,18 +1,19 @@
 DECLARE
     synonym_count NUMBER;
-    table_name_from_all_tables VARCHAR2(100);
 BEGIN
-    SELECT COUNT(*) INTO synonym_count FROM user_synonyms WHERE synonym_name = 'NAZWA_TABELI';
-
-    IF synonym_count = 0 THEN
-        SELECT table_name INTO table_name_from_all_tables FROM all_tables WHERE table_name = 'NAZWA_TABELI';
-        IF table_name_from_all_tables IS NOT NULL THEN
-            EXECUTE IMMEDIATE 'CREATE SYNONYM NAZWA_TABELI FOR ' || table_name_from_all_tables;
-            DBMS_OUTPUT.PUT_LINE('Utworzono synonim dla tabeli.');
+    FOR table_rec IN (SELECT table_name FROM all_tables) LOOP
+        SELECT COUNT(*) INTO synonym_count FROM user_synonyms WHERE synonym_name = table_rec.table_name;
+        
+        IF synonym_count = 0 THEN
+            BEGIN
+                EXECUTE IMMEDIATE 'CREATE SYNONYM ' || table_rec.table_name || ' FOR ' || table_rec.table_name;
+                DBMS_OUTPUT.PUT_LINE('Utworzono synonim dla tabeli ' || table_rec.table_name);
+            EXCEPTION
+                WHEN OTHERS THEN
+                    DBMS_OUTPUT.PUT_LINE('Błąd podczas tworzenia synonimu dla tabeli ' || table_rec.table_name);
+            END;
         ELSE
-            DBMS_OUTPUT.PUT_LINE('Tabela o podanej nazwie nie istnieje.');
+            DBMS_OUTPUT.PUT_LINE('Synonim dla tabeli ' || table_rec.table_name || ' już istnieje.');
         END IF;
-    ELSE
-        DBMS_OUTPUT.PUT_LINE('Synonim dla tabeli już istnieje.');
-    END IF;
+    END LOOP;
 END;
