7b635d9d84ecb4337120a46ba60085b7429fd884
Łukasz <lukasz.f.d@gmail.com>
2024-07-04 14:12:13 +0200

    Update proc_null_check_all_tbl.sql

--- Procedures/proc_null_check_all_tbl.sql
+++ Procedures/proc_null_check_all_tbl.sql
@@ -1,9 +1,9 @@
-CREATE OR REPLACE PROCEDURE check_all_columns_for_nulls(p_table_name IN VARCHAR2) IS
+CREATE OR REPLACE PROCEDURE check_first_row_for_nulls(p_table_name IN VARCHAR2) IS
     v_sql            VARCHAR2(32767);
     v_null_row_count NUMBER;
 BEGIN
-    -- Construct dynamic query to check if all columns in a row are NULL
-    v_sql := 'SELECT COUNT(*) FROM ' || p_table_name || ' WHERE 1=1';
+    -- Construct dynamic query to check if all columns in the first row are NULL
+    v_sql := 'SELECT COUNT(*) FROM (SELECT * FROM ' || p_table_name || ' WHERE ROWNUM = 1) WHERE 1=1';
 
     FOR rec IN (
         SELECT column_name 
@@ -17,7 +17,7 @@ BEGIN
     EXECUTE IMMEDIATE v_sql INTO v_null_row_count;
 
     IF v_null_row_count > 0 THEN
-        RAISE_APPLICATION_ERROR(-20001, 'Table ' || p_table_name || ' contains rows where all columns are NULL.');
+        RAISE_APPLICATION_ERROR(-20001, 'The first row in table ' || p_table_name || ' contains all NULL values.');
     END IF;
 END;
 /
